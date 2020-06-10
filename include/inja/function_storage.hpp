cfec6496104dfa87101a0c0463b9eb607c940073
Tindy X <49061470+tindy2013@users.noreply.github.com>
2020-06-11 01:43:08 +0800

    Add VARARGS support in normal header

--- include/inja/function_storage.hpp
+++ include/inja/function_storage.hpp
@@ -9,6 +9,8 @@
 #include "string_view.hpp"
 
 
+#define INJA_VARARGS (unsigned int) (~0) // use special number for VARARGS functions
+
 namespace inja {
 
 using json = nlohmann::json;
@@ -70,12 +72,15 @@ class FunctionStorage {
       return nullptr;
     }
 
+    const FunctionData* var_func = nullptr;
     for (auto &&i: it->second) {
       if (i.num_args == num_args) {
-        return &i;
+        return &i; // function with precise number of argument(s) should always be used first
+      } else if (i.num_args == INJA_VARARGS) {
+        var_func = &i; // store the VARARGS function for later use
       }
     }
-    return nullptr;
+    return var_func;
   }
 
   std::map<std::string, std::vector<FunctionData>> m_map;
