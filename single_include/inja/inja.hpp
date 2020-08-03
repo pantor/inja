28af9bc0ad7420a3d34be5d062bc0002deb8a582
DeiRex <cs-loosen@hotmail.de>
2020-08-03 18:35:11 +0200

    created single include

--- single_include/inja/inja.hpp
+++ single_include/inja/inja.hpp
@@ -2985,6 +2985,9 @@ class Parser {
           }
 
           function_stack.pop();
+          
+          current_expression_list->rpn_output.emplace_back(operator_stack.top());
+          operator_stack.pop();
         }
       }
       default:
