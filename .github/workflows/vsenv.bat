baa221b9d1225b0a30fa325e1f56cc0ab22de2fd
pantor <1885260+pantor@users.noreply.github.com>
2020-11-25 11:37:32 +0100

    remove set-env in CI

--- .github/workflows/vsenv.bat
+++ .github/workflows/vsenv.bat
@@ -12,6 +12,6 @@ for /f "usebackq delims=*" %%i in (`%VSWHERE% -latest -property installationPath
 :: See: https://stackoverflow.com/questions/39183272/loop-through-all-environmental-variables-and-take-actions-depending-on-prefix
 setlocal
 for /f "delims== tokens=1,2" %%a in ('set') do (
-  echo ::set-env name=%%a::%%b
+  echo %%a=%%b >> $GITHUB_ENV
 )
 endlocal
