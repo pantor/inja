59933e5e6f3bb6969104a5c3bbbcd7f200ec1b75
pantor <lars.berscheid@online.de>
2022-06-20 10:39:56 +0200

    update dependencies

--- single_include/inja/inja.hpp
+++ single_include/inja/inja.hpp
@@ -3,7 +3,7 @@
  |_ _|_ __  (_) __ _    https://github.com/pantor/inja
   | || '_ \ | |/ _` |   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   | || | | || | (_| |
- |___|_| |_|/ |\__,_|   Copyright (c) 2018-2021 Lars Berscheid
+ |___|_| |_|/ |\__,_|   Copyright (c) 2018-2022 Lars Berscheid
           |__/
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
