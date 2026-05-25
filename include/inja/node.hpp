2f927d97cfc6984ad3276f1b64d075aaa1fa8e1e
Rémi Coulom <remi.coulom@gmail.com>
2026-05-25 22:19:19 +0200

    silence Wsuggest-override warning

--- include/inja/node.hpp
+++ include/inja/node.hpp
@@ -268,7 +268,7 @@ class StatementNode : public AstNode {
 public:
   explicit StatementNode(size_t pos): AstNode(pos) {}
 
-  virtual void accept(NodeVisitor& v) const = 0;
+  void accept(NodeVisitor& v) const override = 0;
 };
 
 class ForStatementNode : public StatementNode {
@@ -279,7 +279,7 @@ public:
 
   explicit ForStatementNode(BlockNode* const parent, size_t pos): StatementNode(pos), parent(parent) {}
 
-  virtual void accept(NodeVisitor& v) const = 0;
+  void accept(NodeVisitor& v) const override = 0;
 };
 
 class ForArrayStatementNode : public ForStatementNode {
